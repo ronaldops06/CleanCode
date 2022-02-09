8e063e537a90999d4ce57effd994a3d5bf123eaf
ronaldops06 <ronaldops06@yahoo.com.br>
2022-02-08 21:29:31 -0300

    Tipos diferentes com métodos similares geraram uma nova classe 'ArgumentMarshaler'. Refatoração dos parâmetros do tipo boolean

--- Args/src/com/objectmentor/utilities/args/Args.java
+++ Args/src/com/objectmentor/utilities/args/Args.java
@@ -8,7 +8,7 @@ public class Args {
 	private String[] args;
 	private boolean valid = true;
 	private Set<Character> unexpectedArguments = new TreeSet<Character>();
-	private Map<Character, Boolean> booleanArgs = new HashMap<Character, Boolean>();
+	private Map<Character, ArgumentMarshaler> booleanArgs = new HashMap<Character, ArgumentMarshaler>();
 	private Map<Character, String> stringArgs = new HashMap<Character, String>();
 	private Map<Character, Integer> intArgs = new HashMap<Character, Integer>();
 	private Set<Character> argsFound = new HashSet<Character>();
@@ -69,7 +69,7 @@ public class Args {
 	}
 	
 	private void parseBooleanSchemaElement(char elementId) {
-		booleanArgs.put(elementId, false);
+		booleanArgs.put(elementId, new BooleanArgumentMarshaler());
 	}
 	
 	private void parseIntegerSchemaElement(char elementId) {
@@ -174,7 +174,7 @@ public class Args {
 	}
 	
 	private void setBooleanArg(char argChar, boolean value) {
-		booleanArgs.put(argChar, value);
+		booleanArgs.get(argChar).setBoolean(value);
 	}
 	
 	private boolean isBooleanArg(char argChar) {
@@ -239,7 +239,8 @@ public class Args {
 	}
 	
 	public boolean getBoolean(char arg) {
-		return falseIsNull(booleanArgs.get(arg));
+		Args.ArgumentMarshaler am = booleanArgs.get(arg);
+		return am != null && am.getBoolean();
 	}
 	
 	public boolean has(char arg) {
@@ -251,4 +252,20 @@ public class Args {
 	}
 	
 	private class ArgsException extends Exception {}
+	
+	private class ArgumentMarshaler {
+		private boolean booleanValue = false;
+		
+		public void setBoolean(boolean value) {
+			booleanValue = value;
+		}
+		
+		public boolean getBoolean() { return booleanValue; };
+	}
+	
+	private class BooleanArgumentMarshaler extends ArgumentMarshaler {}
+	
+	private class StringArgumentMarshaler extends ArgumentMarshaler {}
+	
+	private class IntegerArgumentMarshaler extends ArgumentMarshaler {}
 }
